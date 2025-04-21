d5a0e76a6f5bd29734b8320dbc96544e55421921
Maria Paula Morales <mariapaulamoralest13@gmail.com>
2025-04-21 15:43:14 -0600

    mi parte

--- contactUs.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("enviar").addEventListener('click', function mostrarMensaje(e) {
-    e.preventDefault()
-     let nombre = document.getElementById("name").value;
-     let mensaje = "Hola " + nombre + ", gracias por comunicarte con nosotros.";
-     document.getElementById("mensaje-respuesta").innerHTML = mensaje;
- }
- )
